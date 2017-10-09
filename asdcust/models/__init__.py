1576dab1286840dddf3b3dc6f18949465092e923
Jesus Zapata <jesus@vauxoo.com>
2017-10-09 19:14:35 +0000

    10.0-test-jesuszapata: [DEBUG] asdcust: Import pdb

--- asdcust/models/__init__.py
+++ asdcust/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import res_partner
+import pdb
+pdb.set_trace()
