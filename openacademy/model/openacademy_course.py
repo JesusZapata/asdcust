7184754e45b276ae6b773def610d8b52331697fa
JesusZapata <jesus@vauxoo.com>
2017-12-12 13:45:41 +0000

    [FIX] openacademy: Test the empty name in fiedls

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -13,7 +13,7 @@ class Course(models.Model):
     """
     _name = 'openacademy.course'
 
-    name = fields.Char(string='Title', required=True)
+    name = fields.Char(required=True)
     description = fields.Text(string='Description')
     responsible_id = fields.Many2one('res.users',
                                      ondelete='set null', string='Responsible',
